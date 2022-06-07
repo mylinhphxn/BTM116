605c62fba0b8359187909938d08fd27cab10c11d
mylinhphxn <mylinhphxn2@outlook.com>
2022-06-07 15:30:37 -0700

    done

---
+++ conditions1.js
@@ -0,0 +1,11 @@
+console.log("Hello World");
+
+// create variable for random number 0.0-1
+let randomNumber = Math.random();
+
+// if the random number is 
+if (randomNumber < 0.50) {
+
+    console.log("Condition was Met");
+    console.log(randomNumber)
+}
